b6f19378bd973baa13181e7125be9ce15d4d1a6b
shivamsingh12084 <shivamsingh12084@gmail.com>
2022-10-28 02:50:59 +0530

    Initial setup, tailwind configuration and folderStructure setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import Ecommerce from './pages/Ecommerce';
+
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+
+import Orders from './pages/Orders';
+import Employees from './pages/Employees';
+import Customers from './pages/Customers';
+
+import Kanban from './pages/Kanban';
+import Editor from './pages/Editor';
+import Calender from './pages/Calender';
+
+import ColorPicker from './pages/ColorPicker';
+import Line from './pages/Charts/Line';
+import Area from './pages/Charts/Area';
+import Bar from './pages/Charts/Bar';
+import Pie from './components/Charts/Pie';
+import Financial from './pages/Charts/Financial';
+import ColorMapping from './pages/Charts/ColorMapping';
+import Pyramid from './pages/Charts/Pyramid';
+import Stacked from './components/Charts/Stacked';
+
+
+ 
+
+
+import './App.css'
 
 function App() {
+
+    const activeMenu = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowserRouter>
+            <div className='flex relative dark:bg-main-dark-bg'>
+                <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+                    <TooltipComponent content="settings" position="Top">
+                        <button type='button'
+                        className='text 3-xl p-3 hover:dropp-shadow-xl hover:bg-light-gray text-white '
+                        style={{background: 'blue', borderRadius: '50%'}}
+                        >
+                            <FiSettings className='cursor-pointer' />
+                        </button>
+                    </TooltipComponent>
+                </div>
+                
+
+                {activeMenu ? (
+                    <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                        <Sidebar />
+                    </div>
+                ): (
+                    <div className='w-0 dark:bg-secondary-dark-bg'>
+                        <Sidebar />
+                    </div>
+                )}
+
+                <div className={
+                    activeMenu
+                        ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
+                        : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2'
+                }>
+                    <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                        <Navbar />
+                    </div>
+                </div>
+
+                <div>
+                    <Routes>
+                        <Route path="/" element={(<Ecommerce />)} />
+                        <Route path="/ecommerce" element={(<Ecommerce />)} />
+
+                        {/* pages  */}
+                        <Route path="/orders" element={<Orders />} />
+                        <Route path="/employees" element={<Employees />} />
+                        <Route path="/customers" element={<Customers />} />
+
+                        {/* apps  */}
+                        <Route path="/kanban" element={<Kanban />} />
+                        <Route path="/editor" element={<Editor />} />
+                        <Route path="/calendar" element={<Calender />} />
+                        <Route path="/color-picker" element={<ColorPicker />} />
+
+                     {/* charts  */}
+                        <Route path="/line" element={<Line />} />
+                        <Route path="/area" element={<Area />} />
+                        <Route path="/bar" element={<Bar />} />
+                        <Route path="/pie" element={<Pie />} />
+                        <Route path="/financial" element={<Financial />} />
+                        <Route path="/color-mapping" element={<ColorMapping />} />
+                        <Route path="/pyramid" element={<Pyramid />} />
+                        <Route path="/stacked" element={<Stacked />} />
+                    </Routes>
+                </div>
+
+
+            </div>
+        </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Charts/LineChart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LineChart() {
+  return (
+    <div>
+      LineChart
+    </div>
+  )
+}
+
+export default LineChart

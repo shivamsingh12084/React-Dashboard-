c4cfba1f0638fbb7efe593c127bde637b90b9326
shivamsingh12084 <shivamsingh12084@gmail.com>
2022-10-29 06:15:24 +0530

    implementation of Navigation Bar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import "./index.css";
+import { ContextProvider } from './contexts/ContextProvider';
 
 
-ReactDOM.render(<App /> , document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
